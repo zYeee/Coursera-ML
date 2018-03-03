d372d9c1f7f45024932faeed02c4bc6f179948d6
zhuye <yformat930@126.com>
2018-03-03 08:50:54 +0800

    cost function and grad for ex3

--- ex3/ex3/lrCostFunction.m
+++ ex3/ex3/lrCostFunction.m
@@ -9,8 +9,18 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+
+h = sigmoid(X*theta);
+
+J = (-y'*log(h) - (1.-y)'*log(1-h))/m;
+theta(1) = 0;
+
+r = lambda/(2*m)*sum(theta.^2);
+
+J = J + r;
+
+grad = (h-y)'*X/m + ((lambda/m)*theta)';
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -47,6 +57,5 @@ grad = zeros(size(theta));
 
 % =============================================================
 
-grad = grad(:);
 
 end
